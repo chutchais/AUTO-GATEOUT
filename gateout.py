c96f9617c00870bf1a95862dc7bb2117c7c668d8
chutchais <chutchai.s@gmail.com>
2023-09-05 11:21:42 +0700

    Add success sound

--- gateout.py
+++ gateout.py
@@ -28,7 +28,7 @@ width, height = screen.width, screen.height
 qcd = cv2.QRCodeDetector()
 cap = cv2.VideoCapture(camera_id)
 
-                # font
+				# font
 font = cv2.FONT_HERSHEY_SIMPLEX
   
 # org
@@ -48,168 +48,189 @@ color_topic =(255, 79, 51)
 import requests
 import json
 async def send_data(license):
-    print("Start sending...")
-    try :
-        starting_time = time.time()
-        url = f'{RECEIVE_EXPORT_GATE_OUT_URL}?PARM_TRUCKNO={license}&PARM_GATE_ID={GATE_ID}&PARM_LANE_ID={LANE_ID}'
-        # ------------------------------------------------
-        r = requests.post(url, timeout=60)
-        if r.status_code == 200 :
-            r_json = r.json()
-            print(r_json)
-        else:
-            print (f'Error on sending License :{license}')
-        ending_time = time.time()-starting_time
-        time.sleep(1)
-        return f"{r_json['status']} : {r_json['msg']}"
-    except Exception as e:
-        ending_time = time.time()-starting_time
-        return "Error.."
-    
-    
-    print(f"Sending completed...{ending_time:.2f} seconds")
+	print("Start sending...")
+	try :
+		starting_time = time.time()
+		url = f'{RECEIVE_EXPORT_GATE_OUT_URL}?PARM_TRUCKNO={license}&PARM_GATE_ID={GATE_ID}&PARM_LANE_ID={LANE_ID}'
+		# ------------------------------------------------
+		r = requests.post(url, timeout=60)
+		if r.status_code == 200 :
+			r_json = r.json()
+			print(r_json)
+			# Added on Sep 5,2023 -- To play receive EIR sound
+			if r_json['status'] == 'OK' :
+				play_call_sound()
+		else:
+			print (f'Error on sending License :{license}')
+		ending_time = time.time()-starting_time
+		time.sleep(1)
+		return f"{r_json['status']} : {r_json['msg']}"
+	except Exception as e:
+		ending_time = time.time()-starting_time
+		return "Error.."
+	
+	
+	print(f"Sending completed...{ending_time:.2f} seconds")
 
 def print_env(text1,text2,frame):
-    # setup text
-    font = cv2.FONT_HERSHEY_PLAIN
-    # get boundary of this text
-    # textsize = cv2.getTextSize(text, font, 1, 2)[0]
-    footer_start=430
-    textX= 5
-    textY = footer_start+20
-    frame = cv2.putText(frame, text1, (textX, textY ), font, 1, (255, 255, 255), 1)
-    textY = footer_start+40
-    frame = cv2.putText(frame, text2, (textX, textY ), font, 1, (255, 255, 255), 1)
-    # add text centered on image
-    return frame
+	# setup text
+	font = cv2.FONT_HERSHEY_PLAIN
+	# get boundary of this text
+	# textsize = cv2.getTextSize(text, font, 1, 2)[0]
+	footer_start=430
+	textX= 5
+	textY = footer_start+20
+	frame = cv2.putText(frame, text1, (textX, textY ), font, 1, (255, 255, 255), 1)
+	textY = footer_start+40
+	frame = cv2.putText(frame, text2, (textX, textY ), font, 1, (255, 255, 255), 1)
+	# add text centered on image
+	return frame
 
 def print_text_topic(text,background_color,frame):
-    # color = (0, 0, 255)
-    frame = cv2.rectangle(frame, (0,0), (width-1,50), background_color, -1)
-    # setup text
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    # get boundary of this text
-    textsize = cv2.getTextSize(text, font, 1, 2)[0]
+	# color = (0, 0, 255)
+	frame = cv2.rectangle(frame, (0,0), (width-1,50), background_color, -1)
+	# setup text
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	# get boundary of this text
+	textsize = cv2.getTextSize(text, font, 1, 2)[0]
 
-    # get coords based on boundary
-    # textX = (width - textsize[0]) / 2
-    # textY = (50 + textsize[1]) / 2
-    textX= 100
-    textY = 40
+	# get coords based on boundary
+	# textX = (width - textsize[0]) / 2
+	# textY = (50 + textsize[1]) / 2
+	textX= 100
+	textY = 40
 
-    # add text centered on image
-    return cv2.putText(frame, text, (textX, textY ), font, 1, (255, 255, 255), 2)
+	# add text centered on image
+	return cv2.putText(frame, text, (textX, textY ), font, 1, (255, 255, 255), 2)
 
 def print_text_footer(text,background_color,frame):
-    # color = (0, 0, 255)
-    footer_start=430
-    frame = cv2.rectangle(frame, (0,footer_start), (width-1,footer_start+50), background_color, -1)
-    # setup text
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    # get boundary of this text
-    
-    textsize = cv2.getTextSize(text, font, 1, 2)[0]
+	# color = (0, 0, 255)
+	footer_start=430
+	frame = cv2.rectangle(frame, (0,footer_start), (width-1,footer_start+50), background_color, -1)
+	# setup text
+	font = cv2.FONT_HERSHEY_SIMPLEX
+	# get boundary of this text
+	
+	textsize = cv2.getTextSize(text, font, 1, 2)[0]
 
-    # get coords based on boundary
-    # textX = (width - textsize[0]) / 2
-    # textY = (50 + textsize[1]) / 2
-    textX= 5
-    textY = footer_start+40
+	# get coords based on boundary
+	# textX = (width - textsize[0]) / 2
+	# textY = (50 + textsize[1]) / 2
+	textX= 5
+	textY = footer_start+40
 
-    # add text centered on image
-    return cv2.putText(frame, text, (textX, textY ), font, 1, (255, 255, 255), 2)
+	# add text centered on image
+	return cv2.putText(frame, text, (textX, textY ), font, 1, (255, 255, 255), 2)
 
 def detect_qr(sending_data):
-    found_qr = False
-    license = ''
-    ret, frame = cap.read()
+	found_qr = False
+	license = ''
+	ret, frame = cap.read()
 
-    # Add on Aug 29,2023 -- TO flip image to be more sense.
-    # Fliping the image
-    frame = cv2.flip(frame,1)
+	# Add on Aug 29,2023 -- TO flip image to be more sense.
+	# Fliping the image
+	frame = cv2.flip(frame,1)
 
-    # if sending_data :
-    #     return print_text('Sending data...',(255,20,147),frame)
+	# if sending_data :
+	#     return print_text('Sending data...',(255,20,147),frame)
 
-    # color= color = (0, 0, 255)
-    # frame = cv2.rectangle(frame, (0,0), (width-1,50), color, -1)
-    if ret:
-        ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(frame)
-        if ret_qr:
-            for s, p in zip(decoded_info, points):
-                if s:
-                    # Found QR code.
-                    license = s
-                    color = (0, 255, 0)
-                    frame = cv2.putText(frame, f'License : {license}', p[0].astype(int), font, 
-                                fontScale, color, thickness, cv2.LINE_AA)
+	# color= color = (0, 0, 255)
+	# frame = cv2.rectangle(frame, (0,0), (width-1,50), color, -1)
+	if ret:
+		ret_qr, decoded_info, points, _ = qcd.detectAndDecodeMulti(frame)
+		if ret_qr:
+			for s, p in zip(decoded_info, points):
+				if s:
+					# Found QR code.
+					license = s
+					color = (0, 255, 0)
+					frame = cv2.putText(frame, f'License : {license}', p[0].astype(int), font, 
+								fontScale, color, thickness, cv2.LINE_AA)
 
-                    # frame = cv2.putText(frame, f'License : {s}', org, font, 
-                    #             fontScale, color_topic, thickness, cv2.LINE_AA)
-                    
-                    frame = print_text_topic(f'Sending data : {license}',color,frame)
-                    # frame = print_text(msg,(0, 0, 255),frame)
-                    found_qr = True
-                   
-                else:
-                    color = (0, 0, 255)
+					# frame = cv2.putText(frame, f'License : {s}', org, font, 
+					#             fontScale, color_topic, thickness, cv2.LINE_AA)
+					
+					frame = print_text_topic(f'Sending data : {license}',color,frame)
+					# frame = print_text(msg,(0, 0, 255),frame)
+					found_qr = True
+				   
+				else:
+					color = (0, 0, 255)
 
-                frame = cv2.polylines(frame, [p.astype(int)], True, color, thickness)
-                
-        else :
-            if sending_data :
-                msg = 'Sending data...'
-            else:
-                msg = 'Please scan QR code..'
-            # frame = cv2.putText(frame, msg, org, font, 
-            #                     fontScale, color_topic, thickness, cv2.LINE_AA)
-            frame = print_text_topic(msg,(0, 0, 255),frame)
-            # break
-    return frame,found_qr,license
+				frame = cv2.polylines(frame, [p.astype(int)], True, color, thickness)
+				
+		else :
+			if sending_data :
+				msg = 'Sending data...'
+			else:
+				msg = 'Please scan QR code..'
+			# frame = cv2.putText(frame, msg, org, font, 
+			#                     fontScale, color_topic, thickness, cv2.LINE_AA)
+			frame = print_text_topic(msg,(0, 0, 255),frame)
+			# break
+	return frame,found_qr,license
+
+import os
+def play_call_sound():
+	cwd = os.getcwd()
+	sound_root_path = '%s\\sounds\\' % cwd
+	try:
+		from pathlib import Path
+		file_wav = sound_root_path + 'success.wav'
+		file_wav2 = sound_root_path + 'receive_eir.wav'
+		sound_file = Path(file_wav)
+		sound_file2 = Path(file_wav2)
+		from playsound import playsound
+		if sound_file.exists():
+			playsound(file_wav) #Welcome sound
+		if sound_file2.exists():
+			playsound(file_wav2) #Welcome sound
+	except:
+		print ('Error on play_call_sound')
 
 async def main():
-    NUMBER_CAPTURE = 0
-    SENDING_DATA = False
-    send_data_result=''
-    while True:
-        # Capture image
+	NUMBER_CAPTURE = 0
+	SENDING_DATA = False
+	send_data_result=''
+	while True:
+		# Capture image
 
-        frame,found_qr,license = detect_qr(SENDING_DATA)
-        # Show image
+		frame,found_qr,license = detect_qr(SENDING_DATA)
+		# Show image
 
-        cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
-        cv2.moveWindow(window_name, screen.x - 1, screen.y - 1)
-        cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN,
-                            cv2.WINDOW_FULLSCREEN)
-        if send_data_result :
-            frame=print_text_footer(send_data_result,(84,84,84),frame)
-        else:
-            frame=print_env(RECEIVE_EXPORT_GATE_OUT_URL, f'{GATE_ID}:{LANE_ID}',frame)
+		cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+		cv2.moveWindow(window_name, screen.x - 1, screen.y - 1)
+		cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN,
+							cv2.WINDOW_FULLSCREEN)
+		if send_data_result :
+			frame=print_text_footer(send_data_result,(84,84,84),frame)
+		else:
+			frame=print_env(RECEIVE_EXPORT_GATE_OUT_URL, f'{GATE_ID}:{LANE_ID}',frame)
 
-        
-        cv2.imshow(window_name, frame)
+		
+		cv2.imshow(window_name, frame)
 
-        # Added on Sep 1,2023 -- To show monitor screen
-        if SHOW_MONITOR == 'yes' :
-            cv2.imshow(monitor_name, frame)
+		# Added on Sep 1,2023 -- To show monitor screen
+		if SHOW_MONITOR == 'yes' :
+			cv2.imshow(monitor_name, frame)
 
-        # Found Image
-        if found_qr:
-            NUMBER_CAPTURE+=1
-            if NUMBER_CAPTURE == 5:
-                SENDING_DATA = True
-                send_data_result = await send_data(license)
-                SENDING_DATA = False
-        else:
-            # send_data_result=''
-            NUMBER_CAPTURE=0
-            SENDING_DATA = False
-        
+		# Found Image
+		if found_qr:
+			NUMBER_CAPTURE+=1
+			if NUMBER_CAPTURE == 5:
+				SENDING_DATA = True
+				send_data_result = await send_data(license)
+				SENDING_DATA = False
+		else:
+			# send_data_result=''
+			NUMBER_CAPTURE=0
+			SENDING_DATA = False
+		
 
-        if cv2.waitKey(delay) & 0xFF == ord('q'):
-            break
-    cv2.destroyWindow(window_name)
+		if cv2.waitKey(delay) & 0xFF == ord('q'):
+			break
+	cv2.destroyWindow(window_name)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+	asyncio.run(main())
 
